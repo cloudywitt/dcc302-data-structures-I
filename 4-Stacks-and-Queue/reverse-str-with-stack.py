87834a8d13ddde780b117a27bc276fa2e5540a2c
Ângelo Garcia <fhzkngj@gmail.com>
2023-09-08 15:43:06 -0400

    Ex04: add question 1 answer

--- 4-Stacks-and-Queue/reverse-str-with-stack.py
+++ 4-Stacks-and-Queue/reverse-str-with-stack.py
@@ -1,28 +1,53 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
 
 class Stack:
-    def __init__(self, top):
+    def __init__(self, top=None):
         self.top = top
 
-    def peek(self):
-        return self.top.data
+    def is_empty(self):
+        return self.top == None
 
     def push(self, data):
-        ...
+        new_node = Node(data)
+
+        new_node.next = self.top
+        self.top = new_node
     
     def pop(self):
-        ...
+        if self.is_empty():
+            print("The list is empty")
+            
+            return
+
+        item_removed = self.top.data
+        self.top = self.top.next
+
+        return item_removed
 
 
-def reverse_string(str):
-    ...
+def reverse_string(string):
+    if not isinstance(string, str):
+        print("Not a string")
+
+        return
+    
+    str_stack = Stack()
+    str_reversed = ""
+
+    for letter in string:
+        str_stack.push(letter)
+    
+    while not str_stack.is_empty():
+        str_reversed += str_stack.pop()
+
+    return str_reversed
 
 
 if __name__ == "__main__":
-    given_string = input("Enter a string: ", end="")
+    user_string = input("Enter a string: ")
 
-    print(reverse_string(str))
+    print(reverse_string(user_string))
     
